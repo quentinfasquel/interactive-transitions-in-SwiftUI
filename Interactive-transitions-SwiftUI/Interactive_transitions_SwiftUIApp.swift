42bb39e14ab2b59fe21b802d5d29880b4906f5ff
Quentin Fasquel <quentin.fasquel@gmail.com>
2024-03-08 15:35:09 +0100

    Initial Commit

---
+++ Interactive-transitions-SwiftUI/Interactive_transitions_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Interactive_transitions_SwiftUIApp.swift
+//  Interactive-transitions-SwiftUI
+//
+//  Created by Quentin Fasquel on 08/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct Interactive_transitions_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
